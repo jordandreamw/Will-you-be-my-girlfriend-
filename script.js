4483e148c0f16bd02c1d152ed839826e0b3add0f
jordandreamw <jordandreamw@gmail.com>
2024-06-01 09:30:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('noButton').addEventListener('click', function() {
+    alert("Oh, okay... But will you reconsider and click 'Yes'?");
+});
+
+document.getElementById('yesButton').addEventListener('click', function() {
+    alert("Yippie! I'm so happy and excited to create more memories with you as my girlfriend!");
+});
